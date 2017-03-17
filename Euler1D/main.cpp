a06ce19a0d8423edf35a521cb39746970ff4fe70
Andrew Ross <andrew.ross.mail@gmail.com>
2017-03-17 03:14:16 -0400

    Initial Commit

---
+++ Euler1D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler1D
+//
+//  Created by Andrew Ross on 2017-03-17.
+//  Copyright © 2017 Andrew Ross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
